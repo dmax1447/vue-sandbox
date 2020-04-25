e3be81a2d36232eba144944641f7a5025bd5aded
dmax1447 <dmax1447@gmail.com>
2020-04-25 15:01:38 +0300

    рендер вместе со слотами и линком

---
+++ src/components/AncoredHeader.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: 'AncorHeadings',
+  props: {
+    level: Number,
+    href: String,
+  },
+  render(createElement) {
+    console.log('render');
+    console.log(this.href);
+    const slot = this.$slots.default;
+
+    const createLink = (url, text) => createElement(
+      'a',
+      {
+        attrs: {
+          class: 'anc-header__link',
+          href: `#${url}`,
+        },
+      },
+      text,
+    );
+
+    const link = createLink(slot.href, 'link text');
+
+    return createElement(
+      `h${this.level}`,
+      {
+        attrs: {
+          class: 'anc-header__title',
+        },
+      },
+      [link, slot],
+
+    );
+  },
+};
+</script>
---
+++ src/views/Render.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <h1>Render function page</h1>
+    <ancored-header :level="3" href="ancor3">
+      Content passed by slot,
+      another content following,
+      [link-name](http://link-url.com)
+      string content end
+    </ancored-header>
+  </div>
+</template>
+
+<script>
+import AncoredHeader from '../components/AncoredHeader.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    AncoredHeader,
+  },
+};
+</script>
